97689fea38f09436e8c6ff070eec7006599fe3de
wangliyong <wangliyong@youche.com>
2017-07-12 16:58:31 +0800

    更新
    更新

--- netty.pusher.protocol/src/main/java/netty/pusher/protocol/protocol/ExceptionProtocol.java
+++
@@ -1,8 +0,0 @@
-package netty.pusher.protocol.protocol;
-
-import org.msgpack.annotation.Message;
-
-@Message
-public class ExceptionProtocol extends Protocol{
-
-}
--- netty.pusher.protocol/src/main/java/netty/pusher/protocol/protocol/ResponseProtocol.java
+++
@@ -1,8 +0,0 @@
-package netty.pusher.protocol.protocol;
-
-import org.msgpack.annotation.Message;
-
-@Message
-public class ResponseProtocol extends Protocol{
-
-}
